40c5ad773549f36b6591c726e29570ac84176ccb
Nikita Filippov <nikita@jupid.tax>
2025-12-02 16:18:36 +0000

    Update Spotify MacOS plugin manifest and implement Play Playlist action

---
+++ control/src/actions/play-playlist.ts
@@ -0,0 +1,37 @@
+import { action, KeyDownEvent, SingletonAction, WillAppearEvent } from "@elgato/streamdeck";
+import { exec } from "child_process";
+
+@action({ UUID: "com.spotify-macos.control.play-playlist" })
+export class PlayPlaylistAction extends SingletonAction {
+    override async onKeyDown(ev: KeyDownEvent<PlaylistSettings>) {
+        const playlistUri = ev.payload.settings.playlistUri;
+
+        if (!playlistUri) {
+            await ev.action.setTitle("No URI!");
+            return;
+        }
+
+        const script = `
+            tell application "Spotify"
+                play track "${playlistUri}"
+            end tell
+        `;
+
+        exec(`osascript -e '${script}'`, (error) => {
+            if (error) {
+                console.error("Failed to play playlist:", error);
+            }
+        });
+    }
+
+    override async onWillAppear(ev: WillAppearEvent<PlaylistSettings>) {
+        const title = ev.payload.settings.playlistName || "Playlist";
+        await ev.action.setTitle(title);
+    }
+}
+
+type PlaylistSettings = {
+    playlistUri: string;
+    playlistName: string;
+};
+
--- control/src/plugin.ts
+++ control/src/plugin.ts
@@ -1,12 +1,8 @@
 import streamDeck, { LogLevel } from "@elgato/streamdeck";
+import { PlayPlaylistAction } from "./actions/play-playlist";
 
-import { IncrementCounter } from "./actions/increment-counter";
+streamDeck.logger.setLevel(LogLevel.DEBUG);
 
-// We can enable "trace" logging so that all messages between the Stream Deck, and the plugin are recorded. When storing sensitive information
-streamDeck.logger.setLevel(LogLevel.TRACE);
+streamDeck.actions.registerAction(new PlayPlaylistAction());
 
-// Register the increment action.
-streamDeck.actions.registerAction(new IncrementCounter());
-
-// Finally, connect to the Stream Deck.
 streamDeck.connect();
